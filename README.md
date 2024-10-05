970c038e2e2a3c134d4e6a3a5d080e79633618c1
manjeet0796 <manjeetsharma0796@gmail.com>
2024-10-05 21:11:04 +0530

    backend add python

---
+++ README.md
@@ -0,0 +1,8 @@
+# Setup the project using 
+
+yarn install 
+npm install
+
+Add this token as manually into your wallet WRDL
+
+Contract Address = 0xC8F372709416560d1e0D17b806712C592d44FEB2
---
+++ hardhat.config.ts
@@ -0,0 +1,39 @@
+import * as dotenv from "dotenv";
+
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+
+dotenv.config();
+
+const config: HardhatUserConfig = {
+  solidity: {
+    version: "0.8.20",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+      evmVersion: "london",
+    },
+  },
+  networks: {
+    opencampus: {
+      url: `https://open-campus-codex-sepolia.drpc.org`,
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+    scrollTestnet: {
+      url: process.env.SCROLL_TESTNET_URL || "https://rpc.scroll.io",
+      chainId: 534352,
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    },
+    neoTestnet: {
+      url: `https://testnet.rpc.banelabs.org`,
+      accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+      chainId: 12227332,
+      gasPrice: 60000000000,
+    },
+  },
+};
+
+export default config;
